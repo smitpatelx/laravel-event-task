48d27c3900197afa0fbf8b0382e90f7d1ba3cc4d
smitpatelx <smitbpatel1999@gmail.com>
2023-09-22 17:04:34 -0500

    Added timestamp for achievement type as well.

--- database/migrations/2023_09_22_054609_create_achievement_type_table.php
+++ database/migrations/2023_09_22_054609_create_achievement_type_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('achievement_type', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100)->nullable(false)->unique();
+            $table->timestamps();
         });
     }
 
