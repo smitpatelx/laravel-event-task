c024b6fc0a02731fac15a70ea316e4a25ac2ca9b
smitpatelx <smitbpatel1999@gmail.com>
2023-09-22 01:54:08 -0500

    Create badge table

---
+++ database/migrations/2023_09_22_054757_create_badge_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('badge', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 100)->nullable(false);
+            $table->integer('no_of_achievement')->nullable(false);
+            $table->unique(['name', 'no_of_achievement']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('badge');
+    }
+};
