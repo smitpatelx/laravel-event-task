7609ad87ecdf2d2c0b824ce62bdf2a45e0dbd5ec
smitpatelx <smitbpatel1999@gmail.com>
2023-09-22 01:54:37 -0500

    Create achievement user relation table.

---
+++ database/migrations/2023_09_22_054842_create_achievement_user_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achievement_user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('achievement_id')->constrained(
+                table: 'achievement',
+                indexName: 'achievement_id'
+            );
+            $table->foreignId('user_id')->constrained(
+                table: 'users',
+                indexName: 'user_id'
+            );
+            $table->unique(['achievement_id', 'user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('achievement_user', function (Blueprint $table) {
+            $table->dropForeign('achievement_id');
+            $table->dropForeign('user_id');
+        });
+        Schema::dropIfExists('achievement_user');
+    }
+};
