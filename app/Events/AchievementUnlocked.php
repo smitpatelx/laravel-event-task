0a341f71db9a25a6045d1c81f4397d9f2f0ea818
smitpatelx <smitbpatel1999@gmail.com>
2023-09-23 15:22:53 -0500

    Achievement unlocked event added.

---
+++ app/Events/AchievementUnlocked.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\User;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class AchievementUnlocked
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public string $achievement_name;
+    public User $user;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(string $achievement_name, User $user)
+    {
+        $this->achievement_name = $achievement_name;
+        $this->user = $user;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new PrivateChannel('achievement-unlocked'),
+        ];
+    }
+}
