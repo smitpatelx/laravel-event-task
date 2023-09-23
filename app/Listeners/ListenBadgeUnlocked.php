f70aa6c2ae496ae0e4790a5e6d72ea3597452c97
smitpatelx <smitbpatel1999@gmail.com>
2023-09-23 15:46:09 -0500

    Listener for badge unlocked added.

---
+++ app/Listeners/ListenBadgeUnlocked.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\BadgeUnlocked;
+use App\Models\Badge;
+use App\Models\User;
+
+class ListenBadgeUnlocked
+{
+    /**
+     * Handle the event.
+     */
+    public function handle(BadgeUnlocked $event): void
+    {
+        // Store badge unlocked for user
+        $badge = Badge::where('name', $event->badge_name)->first();
+
+        if ($badge !== NULL) {
+            $user = User::find($event->user->id)->first();
+            $user->badge_id = $badge->id;
+            $user->save();
+        }
+    }
+}
