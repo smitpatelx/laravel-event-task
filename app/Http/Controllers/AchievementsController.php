134befede0b6165836a0ef5b7f7ea8f6e66d36e6
smitpatelx <smitbpatel1999@gmail.com>
2023-09-23 15:48:10 -0500

    Achievement controller added. Need to thest the values received after event.

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -3,18 +3,40 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Models\Achievement;
+use App\Models\Badge;
 
 class AchievementsController extends Controller
 {
     public function index(User $user)
     {
+        // Get all achievements that are unlocked
+        $achievements = $user->achievements()->get()->pluck('id')->toArray();
+        // Get all achievements that are not unlocked
+        $next_achievements = Achievement::whereNotIn('id', $achievements)->orderBy('level', 'desc')->get()->pluck('name')->toArray();
+
+        // Get the current badge
+        $badge = $user->badge_id;
+        if ($badge == NULL) {
+            $badge = Badge::first();
+        } else {
+            $badge = Badge::find($badge);
+        }
+
+        // Next badge
+        $next_badge = Badge::where('no_of_achievement', '>', $badge->no_of_achievement)->first();
+        // Next badge name
+        $next_badge_name = $next_badge->name;
+
+        // Remaining to unlock next badge
+        $remaing_to_unlock_next_badge = $next_badge->no_of_achievement - $badge->no_of_achievement;
+
         return response()->json([
-            'unlocked_achievements' => [],
-            'next_available_achievements' => [],
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'unlocked_achievements' => $achievements,
+            'next_available_achievements' => $next_achievements,
+            'current_badge' => $badge,
+            'next_badge' => $next_badge_name,
+            'remaing_to_unlock_next_badge' => $remaing_to_unlock_next_badge,
         ]);
     }
 }
